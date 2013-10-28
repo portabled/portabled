da9c2614f45f76a6a6e12cb6c6dd42744d6c50fe
Oleg Mihailik <mihailik@gmail.com>
2013-10-28 23:47:51 +0000

    Split3.

--- src/layout.ts
+++ src/layout.ts
@@ -1,15 +1,24 @@
+/// <reference path='Split3.ts' />
+
 class Layout {
   private _editorHost = this._document.createElement('div');
   private _fileListHost = this._document.createElement('div');
+
+  private _split: Split3;
+
   constructor(private _host: HTMLElement, private _document: HTMLDocument = document) {
 
     this._fileListHost.textContent = 'OK';
     this._editorHost.textContent = ' [edit] ';
+    var divHost = document.createElement('div');
+    divHost.appendChild(this._fileListHost);
+    divHost.appendChild(this._editorHost);
+    this._split = new Split3(divHost);
+    
 
     Layout.clearContent(this._host);
 
-    this._host.appendChild(this._fileListHost);
-    this._host.appendChild(this._editorHost);
+    this._host.appendChild(divHost);
   }
 
   static clearContent(element: HTMLElement) {
